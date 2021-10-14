2d671f72be7e2b62a15f33ea1c1089c0cda49f70
EuanPyle <euanpyle@gmail.com>
2021-10-14 11:55:03 +0100

    Added minimum markers to input

--- autoalign/dautoalign4relion.m
+++ autoalign/dautoalign4relion.m
@@ -1,10 +1,11 @@
-function dautoalign4relion(ts_dir, apix, fiducial_diameter_nm, nominal_rotation_angle, output_folder)
+function dautoalign4relion(ts_dir, apix, fiducial_diameter_nm, nominal_rotation_angle, min_markers,output_folder)
     %%%%% Automatic on-the-fly alignment of a set of tilt series
     %%% Parameters
     % ts_dir - directory containing tilt series directories
     % apix - pixel size in angstroms of tilt series
     % fiducial_diameter_nm - fiducial diameter in nanometers
     % nominal rotation angle - estimated tilt axis angle (CCW rotation from Y-axis)
+    % Dynamo will delete beads based on worse residuals: this sets the minimum number of beads per tilt before stopping deleting beads. An absolute minimum of 3 is required, but 4 is recommended.
     % output folder - a folder in which to store the 
     
     %%% List of already processed tilt-series
@@ -26,7 +27,7 @@ function dautoalign4relion(ts_dir, apix, fiducial_diameter_nm, nominal_rotation_
         % try to align tilt series
         if isfile(stack)
             try
-                final_dir_name = autoalign(stack, basename, rawtlt, apix, fiducial_diameter_nm, output_folder);
+                final_dir_name = autoalign(stack, basename, rawtlt, apix, fiducial_diameter_nm, min_markers, output_folder);
                 tiltalign(final_dir_name, nominal_rotation_angle, apix)
             catch ME
                 handle_exception(ME)
