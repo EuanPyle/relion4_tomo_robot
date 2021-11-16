421f981ad46b93952089691722105f93cb2f38d8
EuanPyle <euanpyle@gmail.com>
2021-11-16 11:03:03 +0000

    added subfolders for neatness

--- assets/autoalign_activate_example.m
+++ assets/autoalign_activate_example.m
@@ -1,6 +1,8 @@
 install_loc = 'XXX_INSTALL_LOC_XXX';
 PATH = getenv('PATH');
 addpath(install_loc);
+addpath([install_loc '/ctffind_wrappers']);
+addpath([install_loc '/relion_metadata_generators']);
 setenv('PATH', [install_loc, ':' PATH]);
 clear PATH
 disp("Successfully activated 'autoalign_dynamo'!")
--- autoalign/ctffind_wrappers/import_ctffind.m
+++ autoalign/ctffind_wrappers/import_ctffind.m
@@ -1,8 +1,9 @@
+function import_ctffind
 try
 	command = ['cp ' install_loc '/ctffind_wrappers/CTFFIND4_settings_template.csh ./CTFFIND_settings.csh'];
 	system(command);
 catch
-	disp('Set variable install_loc as the install location of this package');
+	disp('Set the variable install_loc as the install location of this package');
 end
 
 
