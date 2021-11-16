536fdf9e7b097abb1bdefd9bbac9a998872a0301
EuanPyle <euanpyle@gmail.com>
2021-11-16 11:08:50 +0000

    Restructured folder and file names

--- assets/autoalign_activate_example.m
+++ assets/autoalign_activate_example.m
@@ -1,6 +1,6 @@
 install_loc = 'XXX_INSTALL_LOC_XXX';
 PATH = getenv('PATH');
-addpath(install_loc);
+addpath([install_loc '/autoalign']);
 addpath([install_loc '/ctffind_wrappers']);
 addpath([install_loc '/relion_metadata_generators']);
 setenv('PATH', [install_loc, ':' PATH]);
